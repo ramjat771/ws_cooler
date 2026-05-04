72ff91f6d19bde45aaf9ca125538680770ec4290
sunita0070 <sunitaq26@gmail.com>
2026-05-04 10:04:12 +0530

    commit

--- server_mcu.mjs
+++ server_mcu.mjs
@@ -3,17 +3,13 @@ import app from "./app.mjs";
 import { PORT } from "./config/env.mjs";
 import { mongoConnection } from "./config/db.mjs";
 import { attachWSServer } from "./ws/wsServer.mjs";
-
 async function startServer() {
   // Create HTTP server
   const server = http.createServer(app);
-
   // 🔥 Attach RAW WebSocket to same server
   attachWSServer(server);
-
   server.listen(PORT, async () => {
     await mongoConnection();
-
     console.log(`🚀 HTTP Server: http://localhost:${PORT}`);
     console.log(`🔌 RAW WS: ws://localhost:${PORT}/?username=device1`);
   });
